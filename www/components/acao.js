b7828b2ea011dc10508a96fff1d11d59d0cb0753
eduardoxns <eduardohue62@gmail.com>
2020-07-15 01:29:35 +0900

    V 1.00.01

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,23 @@
-// This is a JavaScript file
 
+
+$(document).on("click","#buscar",function(){
+  $.ajax({
+    type:"get", //como vou enviar
+    url: "https://viacep.com.br/ws/"+$("#cep").val()+"/json", //para onde enviar
+
+    success: function(data) //executar se der certo
+    {
+      var conteudo;
+      conteudo = "<p><strong>Logradouro: </strong>"+data.logradouro+"</p>"
+      conteudo += "<p><strong>Bairro: </strong>"+data.bairro+"</p>"
+      conteudo += "<p><strong>Cidade: </strong>"+data.localidade+"</p>"
+      conteudo += "<p><strong>Estado: </strong>"+data.uf+"</p>"
+
+      $("#dados").html(conteudo);
+    },
+    error: function() //executar se der erro
+    {
+      alert("Erro");
+    }
+  });
+});
